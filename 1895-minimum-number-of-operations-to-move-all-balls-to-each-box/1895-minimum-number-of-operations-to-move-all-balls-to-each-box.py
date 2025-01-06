d855d87b832d7acda776cef1d27a96f6da64586e
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-01-06 18:48:24 +0530

    Time: 7 ms (89.2%), Space: 17.9 MB (41.05%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,23 +1,20 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
-        res = [0]
+        res = [0]*n
         prefix_sum = 0
 
         count = 1 if boxes[0] == '1' else 0
-
+        count1 = 1 if boxes[-1] == '1' else 0
+        suffix_sum = 0
         for i in range(1,n):
             prefix_sum+=count
-            res.append(prefix_sum)
+            suffix_sum+=count1
+            res[i] +=prefix_sum
+            res[n-i-1]+=suffix_sum
             if boxes[i] == '1':
                 count+=1
-        
-        count = 1 if boxes[-1] == '1' else 0
-        suffix_sum = 0
-        for i in range(n-2,-1,-1):
-            suffix_sum+=count
-            res[i]+=suffix_sum
-            if boxes[i] == '1':
-                count+=1
-        
+            if boxes[n-i-1] == '1':
+                count1+=1
+
         return res

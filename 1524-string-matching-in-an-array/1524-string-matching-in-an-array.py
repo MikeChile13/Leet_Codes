2ac69c0bb73814b7a2838370751eda2cd0f83dee
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-01-07 19:19:19 +0530

    Time: 1 ms (76.5%), Space: 17.8 MB (12.59%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -1,19 +1,11 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         ans = []
-        n = len(words)
-        wordset = set(words)
-
-        def find_substrings(word) -> None:
-            for i in range(len(word)):
-                current = ''
-                for j in range(i,len(word)):
-                    current+=word[j]
-                    if current in wordset and len(current) < len(word):
-                        ans.append(current)
-                        wordset.remove(current)
-        for i in range(n):
-            if words[i] in wordset:
-                find_substrings(words[i])
-
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i == j or len(words[i]) > len(words[j]):
+                    continue
+                if words[i] in words[j]:
+                    ans.append(words[i])
+                    break
         return ans

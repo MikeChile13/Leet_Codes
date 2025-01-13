a9f0a34ad9b9b4b8d001a9f601fc1a6d3e68cf0f
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-01-13 18:20:29 +0530

    Time: 147 ms (41.27%), Space: 18.9 MB (14.92%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
@@ -1,21 +1,16 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        
-        freqs = [0]*26
+        freqs = Counter(s)
         #For each of the characters take the center.
         #if there are two centers add two
         #else add one.
 
         #if count is even, two centers.
         #if count is odd, one center.
-        for char in s:
-            freqs[ord(char)-97]+=1
         res = 0
-        for val in freqs:
-            if val==0:
-                continue
-            elif val %2 == 1:
-                res+=1
+        for val in freqs.values():
+            if val % 2 == 1:
+                res+= 1
             else:
                 res+=2
         return res

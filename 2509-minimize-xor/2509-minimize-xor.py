25030be1727511711beda094f16a45cdf86b8821
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-01-15 20:18:59 +0530

    Time: 3 ms (18.4%), Space: 17.9 MB (23.2%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        def count_set_bits(num):
+            setBits = 0
+            while num > 0:
+                setBits += num & 1
+                num >>= 1
+            return setBits
+        def increase(num,count):
+            pos = 0
+            while count > 0:
+                if num & ( 1 << pos ) == 0:
+                    num |= 1 << pos
+                    count-=1
+                pos+=1
+            return num
+        def decrease(num,count):
+            pos = 0
+            while count > 0:
+                if num & ( 1 << pos ):
+                    num ^= ( 1 << pos ) 
+                    count-=1
+                pos+=1
+            return num
+        num2_Bits,num1_Bits = count_set_bits(num2),count_set_bits(num1)
+
+        if num2_Bits == num1_Bits:
+            return num1
+        elif num2_Bits > num1_Bits:
+            return increase(num1,num2_Bits-num1_Bits)
+        else:
+            return decrease(num1,num1_Bits-num2_Bits)

22597ae134c0015c75da4318f9a597cb966f9721
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-01-19 23:30:53 +0200

    Time: 3 ms (6.07%), Space: 17.6 MB (57.22%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,16 +5,20 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        #initialize length
         length = 1
         proxy = head
+        #find the length of the linked list
         while proxy.next:
             proxy = proxy.next
             length +=1
+        #if the length is equal to n, remove first node
         if n == length:
             return head.next
+        #second proxy is to find the node just before what needs to be cut
         proxy2 = head
         for _ in range(length - n-1):
             proxy2 = proxy2.next
         print(proxy2.val)
-        proxy2.next = proxy2.next.next
+        proxy2.next = proxy2.next.next #cutting out the required node.
         return head

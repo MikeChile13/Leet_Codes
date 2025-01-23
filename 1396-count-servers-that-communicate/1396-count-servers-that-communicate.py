f04c55007932f6d8cc3825562d4ac31b3d8dcf5e
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-01-23 16:00:32 +0200

    Time: 19 ms (52.43%), Space: 19.6 MB (60.67%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
@@ -2,23 +2,18 @@ class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        i = 0
+        Rcount = [0]*n
+        Ccount = [0]*m
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]:
+                    Rcount[j]+=1
+                    Ccount[i]+=1
         res = 0
-        seenRows = set()
-        for row in grid:
-            ones = sum(row)
-            if ones > 1:
-                res+=ones
-                seenRows.add(i)
-            i+=1
-        for column in range(n):
-            actual_ones = 0
-            total_ones = 0
-            for row in range(m):
-                total_ones += grid[row][column]
-                if row not in seenRows:
-                    actual_ones+= grid[row][column]
-            if total_ones > 1:
-                res += actual_ones
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col]:
+                    if Rcount[col] > 1 or Ccount[row] > 1:
+                        res+=1
         return res
     

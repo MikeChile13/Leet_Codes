6e8854ff19608d71b7ed46b276b1f0b35bb67a42
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-01-27 13:02:10 +0200

    Time: 0 ms (100%), Space: 18.1 MB (5.74%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,31 +1,25 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        minv,maxv = -2**31,2**31 - 1
         sign = 1
-        ans = []
-        chars = s.lstrip()
-        print(chars)
-        if not chars:
+        s = s.lstrip()
+        if not s:
             return 0
-        length = len(chars)
-        if chars[0] == '-':
+        i = 0
+        ans = 0
+        if s[i] == '-':
             sign = -1
-            i = 1
-        elif chars[0] == '+':
-            i = 1
-        else:
-            i = 0
-        while i < length and chars[i].isnumeric():
-            ans.append(chars[i])
+            i += 1
+        elif s[0] == '+':
+            i += 1
+        while i < len(s) and s[i].isnumeric():
+            ans = ans * 10 + int(s[i])
             i+=1
-
-        print(chars,ans)
         if not ans:
             return 0
-        ans = int(''.join(ans)) * sign
-        if ans < minv:
-            return minv
-        elif ans > maxv:
-            return maxv
+        ans *= sign
+        if ans < -2**31:
+            return -2**31
+        elif ans > 2**31-1:
+            return 2**31-1
         else:
             return ans

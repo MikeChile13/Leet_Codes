b51562112fde23f6cd24efe0a24b99a81a9ccceb
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-02-01 14:46:11 +0200

    Time: 0 ms (100%), Space: 17.8 MB (22.9%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.py
+++ 3429-special-array-i/3429-special-array-i.py
@@ -1,6 +1,9 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
+        prev = nums[0]&1
         for i in range(1,len(nums)):
-            if nums[i-1]&1 == nums[i]&1:
+            current = nums[i]&1
+            if prev == current:
                 return False
+            prev = current
         return True

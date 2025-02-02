8c230688aa8ac4ad5290cf3430044b62c003c3af
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-02-02 10:51:35 +0200

    Time: 0 ms (100%), Space: 17.8 MB (24.17%) - LeetHub

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.py
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.py
@@ -1,18 +1,5 @@
 class Solution:
     def checkString(self, s: str) -> bool:
-        last_a = 0
-        first_b = 0
-        for i in range(len(s)):
-            if not first_b and s[i] == 'b':
-                first_b = i
-            if s[i] == 'a':
-                last_a = i
-        if not last_a:
-            return True
-        if not first_b and s[0]!='b':
-            return True
-        if last_a < first_b:
-            return True
-        return False
+        return 'ba' not in s
                 
         

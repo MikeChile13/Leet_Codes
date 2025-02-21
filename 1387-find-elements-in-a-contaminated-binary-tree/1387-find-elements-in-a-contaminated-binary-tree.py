210e832269dd339cc3afd94b05733f6b3410598e
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-02-21 18:39:24 +0200

    Time: 19 ms (19.25%), Space: 21.8 MB (93.05%) - LeetHub

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.values = []
+        if root:
+            root.val = 0
+        self.findRealValues(root)
+
+    def find(self, target: int) -> bool:
+        left = 0
+        right = len(self.values)-1
+        
+        while left <= right:
+            mid = left + (right - left)//2
+            if self.values[mid] == target:
+                return True
+            elif target < self.values[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+                
+        return False
+
+
+    def findRealValues(self, node):
+        if node is None:
+            return
+
+        queue = deque([node])
+        while queue:
+            node = queue.popleft()
+            self.values.append(node.val)
+            
+            if node.left:
+                node.left.val = node.val * 2 + 1
+                queue.append(node.left)
+            
+            if node.right:
+                node.right.val = node.val * 2 + 2
+                queue.append(node.right)
+
+        
+        
+        
+
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)

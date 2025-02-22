a69168fd6d79cb1531e4eeb9d268356238fe7484
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-02-22 18:20:18 +0200

    Time: 15 ms (83.82%), Space: 18.6 MB (18.82%) - LeetHub

---
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def __init__(self):
+        self.index = 0
+
+    def createTree(self, prev_height: int, arr: List[int]) -> Optional[TreeNode]:
+        
+        if self.index == len(arr) or prev_height+1 != arr[self.index][0]:
+            return None
+
+        node = TreeNode(arr[self.index][1])
+        self.index+=1
+        node.left = self.createTree(prev_height+1,arr)
+        node.right = self.createTree(prev_height+1,arr)
+
+        return node
+
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        arr = []
+        height = 0
+        index = 0
+        current_num = traversal[0]
+
+        for i in range(1,len(traversal)):
+            if traversal[i].isnumeric():
+                current_num+=traversal[i]
+            elif traversal[i-1].isnumeric():
+                arr.append((height,int(current_num)))
+                height = 1
+                current_num = ''
+            else:
+                height+=1
+
+        arr.append((height,int(current_num)))
+        print(arr)
+        
+        return self.createTree(-1,arr)

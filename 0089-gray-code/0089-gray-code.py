f37e3b9b07237564386ce5b06829b2fa4653e4df
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-02-24 13:34:37 +0200

    Time: 30 ms (16.24%), Space: 26.4 MB (12.46%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.py
@@ -0,0 +1,13 @@
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        if n == 1:
+            return [0,1]
+        base = ['00','01','11','10']
+        for _ in range(2,n):
+            copy = base.copy()
+            for i in range(len(base)):
+                base[i] = '0' + base[i]
+            for i in range(len(base)):
+                copy[i] = '1' + copy[i]
+            base.extend(copy[::-1])
+        return [int(val,2) for val in base]

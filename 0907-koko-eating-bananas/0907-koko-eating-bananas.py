c413f61a18abca033ccc948efaeff8567161ccec
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-02-26 10:57:26 +0200

    Time: 157 ms (58.69%), Space: 18.9 MB (49.39%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,21 +1,21 @@
 class Solution:
     def eatPile(self,k,nums,h):
-        h1 = 0
+        h1 = 0 #h1 will be time taken to eat the pile for given K value
         for bananas in nums:
-            h1 += ceil(bananas/k)
+            h1 += ceil(bananas/k)#ceil because if there's a remainder Koko takes an extra hour to eat the remainder.
             if h1 > h:
-                return False
+                return False # if h1 exceeds we know our K value is too small.
         return True
 
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        top = max(piles)
+        top = max(piles) # the largest K value is the largest in the piles.
 
-        def searchK(left,right):
-            while left <= right:
+        def searchK(left,right): #binary search for K values
+            while left < right:
                 mid = left + (right - left)//2
-                v = self.eatPile(mid,piles,h)
-                if v:
-                    right = mid - 1
+                
+                if self.eatPile(mid,piles,h): 
+                    right = mid
                 else:
                     left = mid + 1
             return left

53668d516321712cced2a327779c4b9e554b0358
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-03-02 17:47:25 +0200

    Time: 1 ms (37.31%), Space: 18.1 MB (17.98%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
@@ -1,15 +1,18 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        id_mapping = defaultdict(int)
-        max_id = 0
-        for id,val in nums1:
-            id_mapping[id] += val
-            max_id = max(max_id,id)
-        for id,val in nums2:
-            id_mapping[id] += val
-            max_id = max(max_id,id)
+        i,j = 0,0
         res = []
-        for i in range(1,max_id+1):
-            if i in id_mapping:
-                res.append([i,id_mapping[i]])
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i][0] < nums2[j][0]:
+                res.append(nums1[i])
+                i += 1
+            elif nums1[i][0] > nums2[j][0]:
+                res.append(nums2[j])
+                j += 1
+            else:
+                res.append([nums1[i][0],nums1[i][1]+nums2[j][1]])
+                j += 1
+                i += 1
+        res.extend(nums1[i:])
+        res.extend(nums2[j:])
         return res

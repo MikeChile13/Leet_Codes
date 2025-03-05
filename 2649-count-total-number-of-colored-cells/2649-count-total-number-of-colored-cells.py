0002d8db88233161f1a9a4616cebc23b2664a2cf
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-03-05 14:08:27 +0200

    Time: 0 ms (100%), Space: 17.9 MB (30.45%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -1,13 +1,13 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-
-    # def findN(N):
     #     if not n:
     #         return 1
     #     return self.coloredCells(n-1) + 4*(n-1)
     #     print(findN(n))
 
-        S = 1
-        for N in range(2,n+1):
-            S = S + 4*(N - 1)
-        return S
+        # S = 1
+        # for N in range(2,n+1):
+        #     S = S + 4*(N - 1)
+        # return S
+        return 2 * (n-1) * n + 1
+

fbe33a4fe8b0886ebcd6dbf47d475b86a99951db
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-03-19 23:50:57 +0200

    Time: 3 ms (97.64%), Space: 19 MB (50.8%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,23 +1,8 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        occurs = [0]*62
-
-        for c in s:
-            if 'a' <= c <= 'z':  # Lowercase letters
-                occurs[ord(c) - ord('a')] += 1
-            elif 'A' <= c <= 'Z':  # Uppercase letters
-                occurs[ord(c) - ord('A') + 26] += 1
-            else:
-                occurs[int(c)+52] += 1
+        occurs = Counter(s)
         max_heap = []
-        for i,v in enumerate(occurs):
-            if v:
-                if i < 26:
-                    c = chr(i+97)
-                elif i < 52:
-                    c = chr(i-26+65)
-                else:
-                    c = str(i-52)
+        for c,v in occurs.items():
                 heapq.heappush(max_heap,(-v,c))
         res = ''
         while max_heap:

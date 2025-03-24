868df4a161d8196e1fe9c591cbbb5710ebde7206
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-03-24 21:14:09 +0200

    Time: 0 ms (100%), Space: 18 MB (14.15%) - LeetHub

--- 2536-determine-if-two-events-have-conflict/2536-determine-if-two-events-have-conflict.py
+++ 2536-determine-if-two-events-have-conflict/2536-determine-if-two-events-have-conflict.py
@@ -1,13 +1,5 @@
 class Solution:
     def haveConflict(self, event1: List[str], event2: List[str]) -> bool:
-        event1 = [
-            float(event1[0][:2]) + float(event1[0][3:])/100,
-            float(event1[1][:2]) + float(event1[1][3:])/100
-            ]
-        event2 = [
-            float(event2[0][:2]) + float(event2[0][3:])/100,
-            float(event2[1][:2]) + float(event2[1][3:])/100
-        ]
         if event1[0] > event2[1] or event1[1] < event2[0]:
             return False
         return True

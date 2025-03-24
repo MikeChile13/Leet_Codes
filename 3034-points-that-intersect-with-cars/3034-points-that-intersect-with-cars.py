15b26e4aae3f378ba3dc98f2d42cd251695cd830
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-03-24 22:34:49 +0200

    Time: 8 ms (20.07%), Space: 17.8 MB (68.24%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.py
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.py
@@ -1,16 +1,18 @@
 class Solution:
     def numberOfPoints(self, nums: List[List[int]]) -> int:
-        point_map = [0]*102
+        point_map = defaultdict(int)
         points = 0
-
-        for start,end in nums:
+        bottom,top = float('inf'),float('-inf')
+        for start, end in nums:
             point_map[start] += 1
             point_map[end + 1] -= 1
+            bottom = min(start,bottom)
+            top = max(end,top)
 
-        for i in range(1,102):
-            point_map[i] += point_map[i-1]
-
-            if point_map[i] != 0:
+        coverage = 0
+        for i in range(bottom, top+1):
+            coverage += point_map[i]
+            if coverage > 0:
                 points += 1
-        
+
         return points

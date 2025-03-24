6460d6eff8cafc17083b19da05b1ad926962de97
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-03-24 22:29:46 +0200

    Time: 11 ms (10.86%), Space: 17.7 MB (92.44%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.py
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.py
@@ -1,16 +1,16 @@
 class Solution:
     def numberOfPoints(self, nums: List[List[int]]) -> int:
-        nums.sort(key=lambda x: x[0])
-        merged = []
-        for i in range(1,len(nums)):
-            if nums[i][0]<= nums[i-1][1]:
-                nums[i][0] = nums[i-1][0]
-                nums[i][1] = max(nums[i][1],nums[i-1][1])
-            else:
-                merged.append(nums[i-1])
-        merged.append(nums[-1])
-        # print(merged)
-        ans = 0
-        for start,end in merged:
-            ans += end - start + 1
-        return  ans
+        point_map = [0]*102
+        points = 0
+
+        for start,end in nums:
+            point_map[start] += 1
+            point_map[end + 1] -= 1
+
+        for i in range(1,102):
+            point_map[i] += point_map[i-1]
+
+            if point_map[i] != 0:
+                points += 1
+        
+        return points

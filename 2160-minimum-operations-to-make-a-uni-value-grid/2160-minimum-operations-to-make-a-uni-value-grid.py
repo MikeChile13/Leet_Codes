45e1fbb6c624758e5422fa64ef66f377511bb59e
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-03-26 21:17:32 +0200

    Time: 148 ms (69.74%), Space: 38.9 MB (78.95%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.py
@@ -7,24 +7,8 @@ class Solution:
                     return -1
                 vals.append(val)
         vals.sort()
-        count = 1
-        left_operations = [0] 
-
-        for i in range(1,len(vals)):
-
-            diff = vals[i] - vals[i-1]
-            opp = diff//x
-
-            left_operations.append(left_operations[i-1]+count*opp)
-            count += 1
-
-        min_val = left_operations[-1]
-        count = 1
-        right_operations = 0
-        for i in range(len(vals)-2,-1,-1):
-            diff = vals[i+1] - vals[i]
-            right_operations += (diff//x) * count 
-            min_val = min(min_val,left_operations[i] + right_operations)
-            count += 1
-        
-        return min_val
+        count = 0
+        median = vals[len(vals)//2]
+        for val in vals:
+            count += abs(median - val)//x
+        return count

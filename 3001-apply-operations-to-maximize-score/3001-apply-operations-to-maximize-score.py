205ffe37855da3cdc83ff1d9e40e33e60ac0a833
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-03-29 22:15:23 +0200

    Time: 3525 ms (42.24%), Space: 43.8 MB (68.1%) - LeetHub

---
+++ 3001-apply-operations-to-maximize-score/3001-apply-operations-to-maximize-score.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        N = len(nums)
+        MOD = 10**9 + 7
+
+        res = 1
+
+        prime_scores = []
+        
+        for n in nums:
+            score = 0
+            for f in range(2,int(sqrt(n))+1):
+                if n % f == 0:
+                    score += 1
+                    while n % f == 0:
+                        n = n // f
+            if n >= 2:
+                score += 1
+            prime_scores.append(score)
+        
+        left_bound = [-1] * N
+        right_bound = [N] * N
+
+        stack = []
+        for i,s in enumerate(prime_scores):
+            while stack and prime_scores[stack[-1]] < s:
+                index = stack.pop()
+                right_bound[index] = i
+            if stack:
+                left_bound[i] = stack[-1]
+            stack.append(i)
+
+        min_heap = [(-n, i) for i, n in enumerate(nums)]
+        heapify(min_heap)
+
+        while k > 0:
+            num, index = heappop(min_heap)
+            num = -num
+            score = prime_scores[index]
+            left_count = index - left_bound[index]
+            right_count = right_bound[index] - index
+
+            count = left_count * right_count
+            operations = min(count,k)
+            res = (res * pow(num,operations,MOD)) % MOD
+            k -= operations
+
+        return res

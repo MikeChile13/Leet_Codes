7644b02ddc63e9740e03527576f8fd0ba2c2f8e0
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-04-01 19:54:00 +0300

    Time: 73 ms (79.07%), Space: 60.9 MB (66.88%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -1,13 +1,12 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         n = len(questions)
-        dp = [0] * n
 
         for i in range(n-1, -1, -1):
             points, jumps = questions[i]
-            take = points + dp[jumps + i + 1] if jumps + i + 1 < n else points
-            not_take = dp[i+1] if i + 1 < n else 0
-            dp[i] = max(take, not_take)
+            take = points + questions[jumps + i + 1][0] if jumps + i + 1 < n else points
+            not_take = questions[i+1][0] if i + 1 < n else 0
+            questions[i][0] = max(take, not_take)
 
-        return dp[0]
+        return questions[0][0]
         

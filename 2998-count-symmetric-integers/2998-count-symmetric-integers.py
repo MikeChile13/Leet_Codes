918f6cde2d7b3462008570704459e18c218b150a
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-04-11 09:54:50 +0300

    Time: 556 ms (68.63%), Space: 17.8 MB (54.99%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.py
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.py
@@ -1,20 +1,20 @@
 class Solution:
+    def isSymmetric(self, num: str) -> bool:
+        n = len(num)
+        if n % 2:
+            return False
+        left = 0
+        right = 0
+        for i in range(n//2):
+            left += int(num[i])
+            right += int(num[n-i-1])
+        return left == right
+        
     def countSymmetricIntegers(self, low: int, high: int) -> int:
+        count = 0
+        for num in range(low,high+1):
+            count += self.isSymmetric(str(num))
 
-        def isSymmetric(num: int) -> bool:
-            s = str(num)
-            n = len(s)
-            if n%2:
-                return False
-            leftSum = 0
-            rightSum = 0
-            for i in range(n//2):
-                leftSum += int(s[i])
-                rightSum += int(s[n-i-1])
-            return leftSum == rightSum
-        result = 0
-        for i in range(low,high+1):
-            if isSymmetric(i):
-                result += 1
+        return count
 
-        return result
+        

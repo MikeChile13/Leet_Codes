331758f205ef07406ba39575ed8e3b50d5f50927
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-04-21 09:37:56 +0300

    Time: 0 ms (100%), Space: 17.9 MB (48.18%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.py
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,10 +1,12 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        #dictionary of groups
         groups = defaultdict(list[int])
         res = []
-        for index,val in enumerate(groupSizes):
-            groups[val].append(index)
-            if len(groups[val]) == val:
+        for index,val in enumerate(groupSizes):#enumerate for index and value
+            groups[val].append(index) #at each point add the value to needed list
+            if len(groups[val]) == val: #deal with lists of same size
                 res.append(groups[val])
                 groups[val] = []
+                #this way the lists of size 3 are separate without hastle 
         return res

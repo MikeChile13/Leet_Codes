0d97c78f32d22022e026ef04674b685666bcd4e8
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-05-01 22:51:24 +0530

    Time: 0 ms (100%), Space: 18.1 MB (8.41%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -1,22 +1,16 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        write_index = 0
-        for i in range(n-1):
-            if not nums[i]:
-                continue
-
+        rse = []
+        for i in range(len(nums)-1):
             if nums[i] == nums[i+1]:
-                nums[i] *=2
+                nums[i] *= 2
                 nums[i+1] = 0
 
-            nums[write_index] = nums[i]
-            if write_index != i:
-                nums[i] = 0
-            write_index += 1
-
-        if nums[-1] and write_index < n - 1:
-            nums[write_index] = nums[-1]
-            nums[-1] = 0
-
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i],nums[index]=nums[index],nums[i]
+                index+=1
+        
         return nums

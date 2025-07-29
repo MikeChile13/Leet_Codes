052275b1cddac713e9f3f6b12cdf21de37c3571d
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-07-30 01:33:54 +0530

    Time: 0 ms (100%), Space: 17.9 MB (12.92%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        prev,curr = 0,0
-        for num in nums:
-            
-            prev,curr = curr,max(curr, prev + num)
-            
-            # print(num,prev,curr)
-        return curr
+        #take and not take, when take skip
+        n = len(nums)
+        # @lru_cache(None)
+        memo = {}
+
+        def dp(i):
+            if i >= n:
+                return 0
+            if i in memo:
+                return memo[i]
+            take = nums[i] + dp(i+2)
+            skip = dp(i+1)
+
+            memo[i] = max(take,skip)
+
+            return memo[i]
+
+        return dp(0)

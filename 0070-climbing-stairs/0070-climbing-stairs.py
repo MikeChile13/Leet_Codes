64d221fd8d3e101ebf156b5d915337d9b178e0ce
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-07-30 00:57:45 +0530

    Time: 0 ms (100%), Space: 17.6 MB (95.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,17 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = defaultdict(int)
+        # memo = defaultdict(int)
+        cache = [-1 for _ in range(n+1)]
         def f(n):
             if n < 0:
                 return 0
 
             if n == 0:
                 return 1
-            if n in memo:
-                return memo[n]
 
-            memo[n] = f(n-1) + f(n-2)
-            
-            return memo[n]
+            # if n not in memo:
+                # memo[n] = f(n-1) + f(n-2)
+            if cache[n] == -1:
+                cache[n] = f(n-1) + f(n-2)
+
+            return cache[n]
 
         return f(n)

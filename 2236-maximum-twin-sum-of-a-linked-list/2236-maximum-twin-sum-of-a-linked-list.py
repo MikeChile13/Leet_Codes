922af90fad4e34bd4f8ed689f08a81180a642172
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-07-31 01:22:59 +0530

    Time: 71 ms (79.71%), Space: 47.8 MB (25.55%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -5,19 +5,21 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        half = []
+        half = []# store half the values
         slow = head
         fast = head
-        while slow and fast:
+        while slow and fast:#slow and fast for the middles of the list
             half.append(slow.val)
             slow = slow.next
             fast = fast.next.next
-        print(slow.val)
-        print(half)
+        # print(slow.val)
+        # print(half)
         maxv = half[-1]
-        n = len(half)
+        n = len(half)-1#using n like this traverses half array in reverse
         while slow:
-            maxv = max(maxv,half[n-1]+slow.val)
+            maxv = max(maxv,half[n]+slow.val)
+            """ instead of adding to the array we  
+            can simply get a max of the twin sum at that node"""
             n-=1
             slow = slow.next
 

53868c8b74575137242b86d5c4aed3a993b6007c
MikeChile13 <143011930+MikeChile13@users.noreply.github.com>
2025-08-19 11:54:54 +0530

    Time: 246 ms (58.23%), Space: 20.1 MB (68.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,7 +1,7 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n = len(grid),len(grid[0])
-        visited = [[False for _  in range(n)] for _ in range(m)]
+        # visited = [[False for _  in range(n)] for _ in range(m)]
 
         # print(visited)
         count = 0
@@ -9,9 +9,9 @@ class Solution:
         dirs = [(0,1),(1,0),(0,-1),(-1,0)]
         def dfs(i,j):
             if 0 <= i < m and 0 <= j < n:
-                if grid[i][j] == '1' and not visited[i][j]:
+                if grid[i][j] == '1': #and not visited[i][j]:
                     grid[i][j] = '0'
-                    visited[i][j] = True
+                    # visited[i][j] = True
                     for a,b in dirs:
                         na = a + i
                         nb = b + j
@@ -21,7 +21,7 @@ class Solution:
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1' and not visited[i][j]:
+                if grid[i][j] == '1': # and not visited[i][j]:
                     count += 1
                     dfs(i,j)
 
